e5d69d45056369d643a07fa031689d349af13be3
nikrus333 <nikrus333@gmail.com>
2024-01-24 13:24:19 +0000

    add cpp code

---
+++ cpp/src/main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+
+
+int main()
+{   
+    std::cout << "hello: " << "world" << "\n";
+    return 0;
+}
