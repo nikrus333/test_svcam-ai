6629748782a90d41c690783e08a3dfe00f182421
nikrus333 <nikrus333@gmail.com>
2024-02-08 15:36:00 +0300

    create ros

--- src/stream_ros2.py
+++ src/stream_ros2.py
@@ -1,6 +1,11 @@
 
 import cv2
 import numpy as np
+import rclpy
+from rclpy.node import Node 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2 
 
 def nothing():
     pass
